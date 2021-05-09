eebd7b18c9b5c1d5020908359784a4ca8e364818
Nicolas Oga <2112.oga@gmail.com>
2021-05-09 14:15:20 -0300

    initial commit

---
+++ src/chapter_1/section_1/Exercise1.java
@@ -0,0 +1,11 @@
+public class Exercise1 {
+  public static void main(String[] _args) {
+    int expressionA = (0 + 15) / 2;
+    double expressionB = 2.0e-6 * 100000000.1;
+    boolean expressionC = true && false || true && true;
+
+    System.out.println("a) " + expressionA);
+    System.out.println("b) " + expressionB);
+    System.out.println("c) " + expressionC);
+  }
+}
---
+++ src/chapter_1/section_1/Exercise2.java
@@ -0,0 +1,13 @@
+public class Exercise2 {
+  public static void main(String[] _args) {
+    double expressionA = (1 + 2.236) / 2;
+    double expressionB = 1 + 2 + 3 + 4.0;
+    boolean expressionC = 4.1 > 4;
+    String expressionD = 1 + 2 + "3";
+
+    System.out.println("a) " + expressionA);
+    System.out.println("b) " + expressionB);
+    System.out.println("c) " + expressionC);
+    System.out.println("d) " + expressionD);
+  }
+}
