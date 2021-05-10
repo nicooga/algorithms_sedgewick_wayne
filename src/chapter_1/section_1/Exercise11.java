c7b56a90473749df481d0cc87e0c58fe5b053ef5
Nicolas Oga <2112.oga@gmail.com>
2021-05-10 18:25:20 -0300

    Imprive ex 1.1.11

--- src/chapter_1/section_1/Exercise11.java
+++ src/chapter_1/section_1/Exercise11.java
@@ -8,28 +8,30 @@ public class Exercise11 {
     int rows = (int) StdRandom.uniform(5, 10);
     int cols = (int) StdRandom.uniform(5, 10);
     boolean[][] matrix = createRandomMatrix(rows, cols);
-    printMatrix(matrix, rows, cols);
+    printMatrix(matrix);
   }
 
   private
 
   static boolean[][] createRandomMatrix(int rows, int cols) {
     boolean[][] matrix = new boolean[rows][cols];
-    fillRandomly(matrix, rows, cols);
+    fillRandomly(matrix);
     return matrix;
   }
 
-  static void fillRandomly(boolean[][] matrix, int rows, int cols) {
-    for (int row = 0; row < rows; row++)
-      for (int col = 0; col < cols; col++)
+  static void fillRandomly(boolean[][] matrix) {
+    for (int row = 0; row < matrix.length; row++)
+      for (int col = 0; col < matrix[row].length; col++)
         matrix[row][col] = StdRandom.bernoulli();
   }
 
-  static void printMatrix(boolean[][] matrix, int rows, int cols) {
+  static void printMatrix(boolean[][] matrix) {
+    int cols = matrix.length;
+
     printHeader(cols);
 
-    for (int row = 0; row < rows; row++)
-      printRow(matrix, row, cols);
+    for (int row = 0; row < cols; row++)
+      printRow(matrix, row);
   }
 
   static void printHeader(int rows) {
@@ -45,7 +47,9 @@ public class Exercise11 {
     StdOut.println(divider);
   }
 
-  static void printRow(boolean[][] matrix, int row, int cols) {
+  static void printRow(boolean[][] matrix, int row) {
+    int cols = matrix[row].length;
+
     StdOut.print(" " + row + " |");
 
     for (int col = 0; col < cols; col++)
