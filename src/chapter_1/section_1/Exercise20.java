31ea75c13c03a29920776c9680219eff28451c9e
Nicolas Oga <2112.oga@gmail.com>
2021-05-12 15:58:12 -0300

    Bastly improved and simplified version of Ex. 1.1.20

--- src/chapter_1/section_1/Exercise20.java
+++ src/chapter_1/section_1/Exercise20.java
@@ -6,17 +6,13 @@ import edu.princeton.cs.algs4.*;
 public class Exercise20 {
   // 1.1.20 Write a recursive static method that computes the value of ln(N!)
   public static void main(String[] _args) {
-    for (int i = 0; i <= 1000; i++)
-      StdOut.printf("factorial of %d is %s\n", i, factorial(i));
+    for (int i = 1; i <= 1000; i++)
+      StdOut.printf("given i is %s, ln(i!) is %s\n", i, lnFactorial(i));
   }
 
-  private static double foo(double n) {
-    return Math.log(factorial(n));
-  }
-
-  private static double factorial(double n) {
-    if (n == 0 || n == 1) return 1;
-    if (n == 2) return n;
-    return factorial(n - 1) * n;
+  private static double lnFactorial(int n) {
+    if (n == 0) throw new Error("Can't compute ln(0)");
+    if (n == 1) return 0;
+    return Math.log(n) + lnFactorial(n - 1);
   }
 }
