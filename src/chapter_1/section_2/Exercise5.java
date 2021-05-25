8dc029e4d0316c0e7f0b4e3435b17480f0f0a0ce
Nicolas Oga <2112.oga@gmail.com>
2021-05-25 20:03:20 -0300

    Ex. 1.2.5

---
+++ src/chapter_1/section_2/Exercise5.java
@@ -0,0 +1,12 @@
+package algsex.chapter1.section2;
+
+import edu.princeton.cs.algs4.*;
+
+public class Exercise5 {
+  public static void main(String[] args) {
+    String s = "Hello World";
+    s.toUpperCase();
+    s.substring(6, 11);
+    StdOut.println(s);
+  }
+}
