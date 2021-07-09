a30b80729d0e29e7994e6c11e593247bd656a46f
Nicolas Oga <2112.oga@gmail.com>
2021-07-09 05:03:24 -0300

    Ex. 1.3.41

--- src/chapter_1/section_3/Exercise41.java
+++ src/chapter_1/section_3/Exercise41.java
@@ -12,5 +12,62 @@ import edu.princeton.cs.algs4.*;
 // Hint: Delete all of the elements from q and add these elements to both q and r.
 public class Exercise41 {
   public static void main(String[] args) {
+    Queue<Integer> q1 = new Queue<>();
+
+    q1.enqueue(1);
+    q1.enqueue(2);
+    q1.enqueue(3);
+
+    Queue<Integer> q2 = new Queue<>(q1);
+
+    assert q2.dequeue() == 1;
+    assert q2.dequeue() == 2;
+    assert q2.dequeue() == 3;
+
+    assert q1.dequeue() == 1;
+    assert q1.dequeue() == 2;
+    assert q1.dequeue() == 3;
+
+    StdOut.println("Tests passed");
+  }
+
+  private static class Queue<T> {
+    private Node first;
+    private Node last;
+
+    Queue() {}
+
+    Queue(Queue<T> original) {
+      Node node = original.first;
+
+      while (node != null) {
+        enqueue(node.item);
+        node = node.next;
+      }
+    }
+
+    public void enqueue(T item) {
+      Node oldLast = last;
+
+      last = new Node();
+      last.item = item;
+
+      if (oldLast == null) first = last;
+      else oldLast.next = last;
+    }
+
+    public T dequeue() {
+      T item = first.item;
+
+      first = first.next;
+      if (first == null) last = null;
+
+      return item;
+    }
+
+    private class Node {
+      T item;
+      Node next;
+    }
   }
 }
