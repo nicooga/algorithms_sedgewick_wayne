ddb27441df08bbd42eeb0b8d718ba2e58fefeb38
Nicolas Oga <2112.oga@gmail.com>
2021-07-09 04:37:33 -0300

    WIP: Ex 1.3.41

---
+++ src/chapter_1/section_3/Exercise41.java
@@ -0,0 +1,16 @@
+package algsex.chapter1.section3;
+
+import java.util.*;
+import edu.princeton.cs.algs4.*;
+
+// 1.3.41 Copy a queue. Create a new constructor so that
+//
+//     Queue<Item> r = new Queue<Item>(q);
+//
+// makes r a reference to a new and independent copy of the queue q. You should be able
+// to enqueue and dequeue from either q or r without influencing the other.
+// Hint: Delete all of the elements from q and add these elements to both q and r.
+public class Exercise41 {
+  public static void main(String[] args) {
+  }
+}
