3ed66c22829c76705f65e784c7bf46056bbe4b39
Nicolas Oga <2112.oga@gmail.com>
2021-07-12 07:10:07 -0300

    WIP Ex. 1.3.47: stack concatenation

--- src/chapter_1/section_3/Exercise47.java
+++ src/chapter_1/section_3/Exercise47.java
@@ -20,17 +20,39 @@ public class Exercise47 {
     assert q2.dequeue() == 5;
     assert q2.dequeue() == 6;
 
-    Queue<Integer> q4 = buildQueue(1);
-    Queue<Integer> q5 = buildQueue(4);
+    Queue<Integer> q3 = buildQueue(1);
+    Queue<Integer> q4 = buildQueue(4);
+    Queue<Integer> q5 = q3.concat(q4);
 
-    Queue<Integer> q6 = q4.concat(q5);
+    assert q5.dequeue() == 1;
+    assert q5.dequeue() == 2;
+    assert q5.dequeue() == 3;
+    assert q5.dequeue() == 4;
+    assert q5.dequeue() == 5;
+    assert q5.dequeue() == 6;
 
-    assert q6.dequeue() == 1;
-    assert q6.dequeue() == 2;
-    assert q6.dequeue() == 3;
-    assert q6.dequeue() == 4;
-    assert q6.dequeue() == 5;
-    assert q6.dequeue() == 6;
+    Stack<Integer> s1 = buildStack(1);
+
+    assert s1.pop() == 3;
+    assert s1.pop() == 2;
+    assert s1.pop() == 1;
+
+    Stack<Integer> s2 = buildStack(4);
+
+    assert s2.pop() == 6;
+    assert s2.pop() == 5;
+    assert s2.pop() == 4;
+
+    Stack<Integer> s3 = buildStack(1);
+    Stack<Integer> s4 = buildStack(4);
+    Stack<Integer> s5 = s3.concat(s4);
+
+    assert s4.pop() == 3;
+    assert s4.pop() == 2;
+    assert s4.pop() == 1;
+    assert s4.pop() == 6;
+    assert s4.pop() == 5;
+    assert s4.pop() == 4;
 
     StdOut.println("Tests passed");
   }
@@ -43,6 +65,16 @@ public class Exercise47 {
     q.enqueue(firstItem + 2);
 
     return q;
+  }
+
+  private static Stack<Integer> buildStack(int firstItem) {
+    Stack<Integer> s = new Stack();
+
+    s.push(firstItem);
+    s.push(firstItem + 1);
+    s.push(firstItem + 2);
+
+    return s;
   }
 
   private static class Queue<T> {
@@ -78,6 +110,49 @@ public class Exercise47 {
       anotherQueue.last.next = thisFirst;
 
       last = anotherQueue.last;
+
+      return this;
+    }
+
+    private class Node {
+      T item;
+      Node next;
+    }
+  }
+
+  private static class Stack<T> {
+    private Node last;
+
+    public void push(T item) {
+      Node first = new Node();
+      first.item = item;
+
+      if (last == null) {
+        first.next = first;
+        last = first;
+      } else {
+        first.next = last.next;
+        last.next = first;
+      }
+    }
+
+    public T pop() {
+      T item = last.next.item;
+
+      if (last.next == last) last = null;
+      else last.next = last.next.next;
+
+      return item;
+    }
+
+    public Stack<T> concat(Stack<T> anotherStack) {
+      Node thisFirst = last.next;
+      Node anotherFirst = anotherStack.last.next;
+
+      last.next = anotherFirst;
+      anotherStack.last.next = thisFirst;
+
+      last = anotherStack.last;
 
       return this;
     }
