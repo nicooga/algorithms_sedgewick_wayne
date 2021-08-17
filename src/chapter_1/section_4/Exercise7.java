49a5648932a22252d565b4195dd925410956be8c
Nicolas Oga <2112.oga@gmail.com>
2021-08-17 16:55:33 -0300

    Ex. 1.4.7

--- src/chapter_1/section_4/Exercise7.java
+++ src/chapter_1/section_4/Exercise7.java
@@ -4,38 +4,43 @@ import java.util.*;
 import edu.princeton.cs.algs4.*;
 import algsex.support.Rational;
 
+// 1.4.7 Analyze ThreeSum under a cost model that counts arithmetic operations (and
+// comparisons) involving the input numbers.
 public class Exercise7 {
   public static void main(String[] args) {
-    test(new int[] { 0, 1, -1, 2, 3, 4, 5, 6, 7, 8, 9, 2 }, 1);
+    assertCorrect(new int[] { 0, 1, -1, 2, 3, 4, 5, 6, 7, 8, 9, 2 }, 1);
+    assertCorrect(new int[] { 0, 1, -1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 23 }, 1);
+    assertCorrect(new int[] { 0, 1, -1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 23, -2, -3 }, 9);
+
+    StdOut.println("Tests passed");
   }
 
-  private static void test(int[] a, int matchingPairs) {
-    int expectedOperations = expectedOperations(a, matchingPairs);
+  private static void assertCorrect(int[] a, int expectedTripleCount) {
+    int actualTripleCount = InstrumentedThreeSum.count(a);
 
-    assert InstrumentedThreeSum.count(a) == 1;
+    assert actualTripleCount == expectedTripleCount :
+      String.format("Expected triple count to be %s, but was %s.", expectedTripleCount, actualTripleCount);
 
+    int expectedOperations = expectedOperations(a, expectedTripleCount);
     int actualOperations = InstrumentedThreeSum.operations;
 
     assert actualOperations == expectedOperations :
       String.format("Expected %s operations, but was %s.", expectedOperations, actualOperations); 
   }
 
-  private static int expectedOperations(int[] a, int matchingPairs) {
+  private static int expectedOperations(int[] a, int expectedTripleCount) {
     int N = a.length;
 
-    Rational twoThirdsOfNCubed = r(2, 3).times(r(Math.pow(N, 3)));
-    Rational threeHalvesNSquared = r(3, 2).times(r(Math.pow(N, 2)));
-    Rational elevenSixthsN = r(11, 6).times(r(N));
-
     return
-      twoThirdsOfNCubed
-        .minus(threeHalvesNSquared)
-        .plus(elevenSixthsN)
-        .plus(r(matchingPairs))
+      r(2, 3).times(r(Math.pow(N, 3)))
+        .minus(r(3, 2).times(r(Math.pow(N, 2))))
+        .plus(r(11, 6).times(r(N)))
+        .plus(r(expectedTripleCount))
         .toInteger();
   }
 
-  private static Rational r(int a, int b) { return new Rational(a, b); }
+  private static Rational r(double a, int b) { return new Rational((long) a, (long) b); }
+  private static Rational r(int a, int b) { return new Rational((long) a, (long) b); }
   private static Rational r(int a) { return new Rational((long) a, (long) 1); }
   private static Rational r(double a) { return new Rational((long) a, (long) 1); }
 
@@ -52,17 +57,17 @@ public class Exercise7 {
         for (int j = i+1; j < N; j++) { // for each i, j is incremented (N-1) - (i + 1) times
           for (int k = j+1; k < N; k++) { // for each j, k is incremented (N - 1) - (j + 1) times
             if (a[i] + a[j] + a[k] == 0) { // for each k, 2 sums and and one comparison ocur
-              cnt++; // depends on the number of triples found
-              // operations++;
+              cnt++;
+              operations++; // depends on the number of triples found
             }
 
-            operations += 4;
+            operations += 4; // k increments, sums and comparisons
           }
 
-          operations++;
+          operations++; // j increments
         }
 
-        operations++;
+        operations++; // i increments
       }
   
       return cnt;
--- src/chapter_1/section_4/Exercise7.md
+++ src/chapter_1/section_4/Exercise7.md
@@ -22,3 +22,34 @@ public class ThreeSum {
     }
 }
 ~~~
+
+Let's assume that "operations" includes increments to counter variables like `i`.
+
+The `int i` variable is incremented `N` times.
+
+The `int j` variable is incremented `(N - i - 1)` times for each value of `i`, which goes from `0` to `N - 1`:
+
+![expression 1](./Exercise7/expression1.png)  
+![expression 2](./Exercise7/expression2.png)  
+
+Working out this expression gives us:
+
+![expression 3](./Exercise7/expression3.png) 
+
+The `int k` variable is incremented `(N - i - 1)` times for each value of `k` and `i`. `a[i] + a[j] + a[k] == 0` means 2 sum operations and a comparison for the same amount of times. This is the same as saying '4 sum operations are performed for each iteration of the main loop".
+
+We can calculate the frequency of execution of the main loop by working out this expression:
+
+![expression 4](./Exercise7/expression4.png) 
+
+... which after a lot of massaging gives us:
+
+![expression 5](./Exercise7/expression5.png).
+
+Summing up all these expressions for a grand total gives us:
+
+![expression 6](./Exercise7/expression6.png)  
+
+Finally, for each matching triple, the operation `cnt++` is executed. We can just sum this value `x` to the expression to account for that.
+
+`Exercise7.java` whithin this same folder uses an instrumented version of `ThreeSum` and a few test cases to confirm this expression is correct.
