586ab69c7432e4959b93bcbb93c1eb272278ae3c
Nicolas Oga <2112.oga@gmail.com>
2021-08-19 13:54:50 -0300

    Ex. 1.4.10

--- src/chapter_1/section_4/Exercise10.java
+++ src/chapter_1/section_4/Exercise10.java
@@ -33,23 +33,22 @@ public class Exercise10 {
   }
 
   private static class BinarySearch {
-    public static int rank(int key, int[] a)  {
-      int lo = 0;
-      int hi = a.length - 1;
+    public static int rank(int key, int[] a) {
+      return rank(key, a, 0, a.length - 1);
+    }
 
-      while (lo <= hi) {
-        int mid = (lo + hi) / 2;
+    public static int rank(int key, int[] a, int lo, int hi)  {
+      if (lo > hi) return -1;
 
-        if (a[lo] == key) return lo;
-        else if (a[mid] < key) lo = mid+1;
-        else if (a[mid] > key) hi = mid-1;
-        else {
-          while (a[mid-1] == key) mid--;
-          return mid;
-        }
+      int mid = (lo + hi) / 2;
+
+      if (a[mid] < key) return rank(key, a, mid+1, hi);
+      else if (a[mid] > key) return rank(key, a, lo, mid-1);
+      else {
+        int potentialLowerDup = rank(key, a, lo, mid-1);
+        if (potentialLowerDup != -1) return potentialLowerDup;
+        return mid;
       }
-
-      return -1;
     }
   }
 }
