3fe200c0a6f2757c395ecd76c37aa4a48c564eee
Nicolas Oga <2112.oga@gmail.com>
2021-09-11 16:37:26 -0300

    WIP: 1.4.25

--- src/chapter_1/section_4/Exercise25.java
+++ src/chapter_1/section_4/Exercise25.java
@@ -11,48 +11,54 @@ import algsex.support.Test;
 // search hits (egg intact) are much cheaper than misses (egg broken).
 public class Exercise25 {
     public static void main(String[] args) {
-        // for (int i = 4; i > 0; i *= 2)
-        //     runExperiment(i, i);
+        MainTest.main();
+        StdOut.println("Tests passed");
+    }
 
-        // for (int i = 4; i > 0; i *= 2)
-        //     runExperiment(i);
-
-        // StdOut.println("Tests passed");
-
-        int N = 100;
-        for (int F = 1; F < N; F++) {
-            EggThrowExperiment experiment = new EggThrowExperiment(N, F);
-            assert experiment.maxSafeThrowHeight() == F;
-            StdOut.println(experiment.eggThrows);
+    private static class MainTest {
+        public static void main() {
+            for (int N = 4; N > 0; N *= 2) {
+                runExperiment(N, 1);
+                runExperiment(N, N);
+                runExperiment(N);
+            }
         }
-    }
 
-    private static void runExperiment() {
-        int N = StdRandom.uniform(10, 1000);
-        runExperiment(N);
-    }
+        private static void runExperiment() {
+            int N = StdRandom.uniform(10, 1000);
+            runExperiment(N);
+        }
 
-    private static void runExperiment(int N) {
-        int F = StdRandom.uniform(1, N+1);
-        runExperiment(N, F);
-    }
+        private static void runExperiment(int N) {
+            int F = StdRandom.uniform(1, N+1);
+            runExperiment(N, F);
+        }
 
-    private static void runExperiment(int N, int F) {
-        EggThrowExperiment experiment = new EggThrowExperiment(N, F);
-        int actualF = experiment.maxSafeThrowHeight();
-        int maxThrows = (int) (6 * Math.sqrt(F));
-        int actualThrows = experiment.eggThrows;
+        private static void runExperiment(int N, int F) {
+            EggThrowExperiment experiment = new EggThrowExperiment(N, F);
+            int actualF = experiment.maxSafeThrowHeight();
+            int maxThrows = (int) (4 * Math.sqrt(F));
+            int expectedThrows = expectedThrows(N, F);
+            int actualThrows = experiment.eggThrows;
 
-        try {
-            Test.assertLessThanOrEqual(actualF, F, "expected F to be " + F + " but it was " + actualF);
-            Test.assertLessThanOrEqual(actualThrows, maxThrows, "expected throws to be less than or equal to " + maxThrows + " they were " + actualThrows);
-        } catch (AssertionError e) {
-            StdOut.printf(
-                "Experiment failed for N=%s, F=%s, expected max throws: %s, actual throws: %s\n",
-                N, F, maxThrows, actualThrows
-            );
+            try {
+                Test.assertLessThanOrEqual(actualF, F, "expected F to be " + F + " but it was " + actualF);
+                Test.assertLessThanOrEqual(actualThrows, maxThrows, "expected throws to be less than or equal to " + maxThrows + " they were " + actualThrows);
+                Test.assertLessThanOrEqual(actualThrows, expectedThrows, "expected throws to be " + expectedThrows + " they were " + actualThrows);
+            } catch (AssertionError e) {
+                StdOut.printf(
+                    "Experiment failed for N=%s, F=%s, expected max throws: %s, actual throws: %s\n",
+                    N, F, maxThrows, actualThrows
+                );
 
-            throw e;
+                throw e;
+            }
+        }
+
+        private static int expectedThrows(int N, int F) {
+            int ti = TriangularNumber.indexOfTriangularNumberEqualToOrGreaterThan(N);
+            int l = TriangularNumber.layerFromBottomFor(F, ti);
+            return l + Math.min(F - TriangularNumber.partialSumForLayersUpTo(l-1, ti), TriangularNumber.numberCountForLayer(l, ti)-1);
         }
     }
 
@@ -71,7 +77,7 @@ public class Exercise25 {
         }
 
         public int maxSafeThrowHeight() {
-            int tIndex = indexOfTriangularNumberEqualOrGreaterThan(N);
+            int tIndex = TriangularNumber.indexOfTriangularNumberEqualToOrGreaterThan(N);
             int lo = 1;
             int hi = (int) tIndex;
 
@@ -95,17 +101,40 @@ public class Exercise25 {
             throw new RuntimeException("This should not have happened");
         }
 
-        // 1 indexed. E.g: 1st = 1, 2nd = 3, 3rd = 6, 4th = 10, 5th = 15, and so on.
-        private static int indexOfTriangularNumberEqualOrGreaterThan(int y) {
-            return (int) Math.ceil(Math.sqrt(2 * y + 1/4) - 1/2.0);
-        }
-
         private boolean eggBreaksWhenThrownFrom(int h) {
             assert eggsLeft > 0;
             eggThrows++;
             boolean broken = h >= F;
             if (broken) eggsLeft--;
             return broken;
+        }
+    }
+
+    // ti stands for "triangular number index", which happens to be the same as the length of the biggest row in any triangular number
+    private static class TriangularNumber {
+        // 1 indexed. E.g: 1st = 1, 2nd = 3, 3rd = 6, 4th = 10, 5th = 15, and so on.
+        public static int indexOfTriangularNumberEqualToOrGreaterThan(int x) {
+            return (int) Math.ceil(indexOfTriangularNumberEqualTo(x));
+        }
+
+        private static double indexOfTriangularNumberEqualTo(int x) {
+            return Math.sqrt(2 * x + 1/4.0) - 1/2.0;
+        }
+
+        private static int triangularNumberAtIndex(int i) {
+            return (int) ((Math.pow(i, 2) + i) / 2.0);
+        }
+
+        public static int layerFromBottomFor(int x, int ti) {
+            return (int) Math.ceil(ti + 1/2.0 - Math.sqrt(Math.pow(ti + 1/2.0, 2) - 2*x));
+        }
+
+        public static int partialSumForLayersUpTo(int layer, int ti) {
+            return (int) (layer * ti - Math.pow(layer, 2) / 2.0 + layer / 2.0);
+        }
+
+        public static int numberCountForLayer(int l, int ti) {
+            return ti - l + 1;
         }
     }
 }
