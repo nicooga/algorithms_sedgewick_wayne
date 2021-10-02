d9631cdc32e2800fb63bd1fe4a5ac075053cc60d
Nicolas Oga <2112.oga@gmail.com>
2021-10-02 17:22:21 -0300

    WIP: 1.4.40

---
+++ src/chapter_1/section_4/Exercise40.java
@@ -0,0 +1,13 @@
+package algsex.chapter1.section4;
+
+import java.util.*;
+import edu.princeton.cs.algs4.*;
+
+// 1.4.40 3-sum for random values. Formulate and validate a hypothesis describing the
+// number of triples of N random int values that sum to 0. If you are skilled in math-
+// ematical analysis, develop an appropriate mathematical model for this problem, where
+// the values are uniformly distributed between –M and M, where M is not small.
+public class Exercise40 {
+    public static void main(String[] args) {
+    }
+}
