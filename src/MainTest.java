808c13fd124db3890dfa3174fa13e7d481d24a0e
Nicolas Oga <2112.oga@gmail.com>
2021-11-24 13:22:02 -0300

    Add chapter 1 sectiion 2 tests to CI

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,9 +1,12 @@
 package algsex;
 
+import algsex.support.*;
+
 public class MainTest {
     private static final String[] EMPTY_ARGS = new String[] {};
 
     public static void main(String[] args) {
-        algsex.chapter1.section1.MainTest.main(EMPTY_ARGS);
+        algsex.chapter1.section1.MainTest.main(Test.emptyArgs());
+        algsex.chapter1.section2.MainTest.main(Test.emptyArgs());
     }
 }
