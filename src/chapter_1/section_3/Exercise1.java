c3201a1b310db256d0110c7cbe8a48d46a1e4998
Nicolas Oga <2112.oga@gmail.com>
2021-11-24 23:45:46 -0300

    Refactor Ex. 1.3.1

--- src/chapter_1/section_3/Exercise1.java
+++ src/chapter_1/section_3/Exercise1.java
@@ -2,37 +2,38 @@ package algsex.chapter1.section3;
 
 import java.util.*;
 import edu.princeton.cs.algs4.*;
+import algsex.support.Test;
 
+// 1.3.1 Add a method isFull() to FixedCapacityStackOfStrings.
 public class Exercise1 {
-  public static void main(String[] args) {
-    FixedCapacityStackOfStrings stack = new FixedCapacityStackOfStrings(10);
+    public static void main(String[] _args) {
+        int N = 3;
+        FixedCapacityStackOfStrings stack = new FixedCapacityStackOfStrings(N);
 
-    while(!StdIn.isEmpty()) {
-      String line = StdIn.readLine().trim();
+        for (int i = 0; i < N; i++) {
+            Test.assertFalse(stack.isFull());
+            stack.push(Integer.toString(i));
+        }
 
-      if (line.equals("-"))
-        StdOut.println(stack.pop());
-      else if (stack.isFull())
-        StdOut.println("stack is full");
-      else
-       stack.push(line);
+        Test.assertTrue(stack.isFull());
+        Test.assertRaises(ArrayIndexOutOfBoundsException.class, () -> stack.push(""));
+        Test.assertTrue(stack.isFull());
     }
-  }
 
-  private static class FixedCapacityStackOfStrings {
-    private String[] a; // stack entries
-    private int N; // size
-    private int cap;
+    private static class FixedCapacityStackOfStrings {
+        private String[] a; // stack entries
+        private int N; // size
+        private int cap;
 
-    public FixedCapacityStackOfStrings(int cap) {
-      this.cap = cap;
-      this.a = new String[cap];
-     }
+        public FixedCapacityStackOfStrings(int cap) {
+            this.cap = cap;
+            this.a = new String[cap];
+        }
 
-    public boolean isEmpty() { return N == 0; }
-    public int size() { return N; }
-    public void push(String item) { a[N++] = item; }
-    public String pop() { return a[--N]; }
-    public boolean isFull() { return N >= cap - 1; }
-  }
+        public boolean isEmpty() { return N == 0; }
+        public int size() { return N; }
+        public void push(String item) { a[N++] = item; }
+        public String pop() { return a[--N]; }
+        public boolean isFull() { return N >= cap; }
+    }
 }
