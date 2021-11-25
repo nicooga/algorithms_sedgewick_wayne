d9566463f1b3051b0a78836ecc45fa74f971bffb
Nicolas Oga <2112.oga@gmail.com>
2021-11-24 23:46:13 -0300

    Refactor Ex. 1.3.2

--- src/chapter_1/section_3/Exercise2.java
+++ src/chapter_1/section_3/Exercise2.java
@@ -1,60 +1,35 @@
 package algsex.chapter1.section3;
 
-import java.util.*;
 import edu.princeton.cs.algs4.*;
+import algsex.support.Stack;
 
+// 1.3.2 Give the output printed by java Stack for the input
+// it was - the best - of times - - - it was - the - -
 public class Exercise2 {
-  public static void main(String[] args) {
-    Stack<String> s = new Stack<String>();
-
-    consumeLine(s, "it");    // it
-    consumeLine(s, "was");   // it was
-    consumeLine(s, "-");     // it (was)
-    consumeLine(s, "the");   // it the
-    consumeLine(s, "best");  // it the best
-    consumeLine(s, "-");     // it the (best)
-    consumeLine(s, "of");    // it the of
-    consumeLine(s, "times"); // it the of times
-    consumeLine(s, "-");     // it the of (times)
-    consumeLine(s, "-");     // it the (of)
-    consumeLine(s, "-");     // it (the)
-    consumeLine(s, "it");    // it it
-    consumeLine(s, "was");   // it it was
-    consumeLine(s, "-");     // it it (was)
-    consumeLine(s, "the");   // it it the
-    consumeLine(s, "-");     // it it (the)
-    consumeLine(s, "-");     // it (it)
-  }
-
-  private static void consumeLine(Stack<String> stack, String line) {
-    if (!line.equals("-")) stack.push(line);
-    else if (!stack.isEmpty()) StdOut.println(stack.pop() + " ");
-  }
-
-  private static class Stack<Item> {
-    private Node first;
-    private int N;
-
-    private class Node {
-      Item item;
-      Node next;
+    public static void main(String[] args) {
+        Stack<String> s = new Stack<String>();
+    
+        consumeLine(s, "it");    // it
+        consumeLine(s, "was");   // it was
+        consumeLine(s, "-");     // it (was)
+        consumeLine(s, "the");   // it the
+        consumeLine(s, "best");  // it the best
+        consumeLine(s, "-");     // it the (best)
+        consumeLine(s, "of");    // it the of
+        consumeLine(s, "times"); // it the of times
+        consumeLine(s, "-");     // it the of (times)
+        consumeLine(s, "-");     // it the (of)
+        consumeLine(s, "-");     // it (the)
+        consumeLine(s, "it");    // it it
+        consumeLine(s, "was");   // it it was
+        consumeLine(s, "-");     // it it (was)
+        consumeLine(s, "the");   // it it the
+        consumeLine(s, "-");     // it it (the)
+        consumeLine(s, "-");     // it (it)
     }
-
-    public boolean isEmpty() { return first == null; }
-    public int size() { return N; }
-    public void push(Item item) {
-      Node oldfirst = first;
-      first = new Node();
-      first.item = item;
-      first.next = oldfirst;
-      N++;
+  
+    private static void consumeLine(Stack<String> stack, String line) {
+        if (!line.equals("-")) stack.push(line);
+        else if (!stack.isEmpty()) StdOut.println(stack.pop() + " ");
     }
-
-    public Item pop() {
-      Item item = first.item;
-      first = first.next;
-      N--;
-      return item;
-    }
-  }
 }
