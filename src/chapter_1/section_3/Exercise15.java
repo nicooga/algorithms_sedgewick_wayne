7b9f00d150751f645cd3d9bd33e4f73dffdaa05d
Nicolas Oga <2112.oga@gmail.com>
2021-11-25 17:11:05 -0300

    Refactor Ex. 1.3.15 to add automated test

--- src/chapter_1/section_3/Exercise15.java
+++ src/chapter_1/section_3/Exercise15.java
@@ -1,21 +1,51 @@
 package algsex.chapter1.section3;
 
-import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.Queue;
+import algsex.support.*;
 
 // 1.3.15 Write a Queue client that takes a command-line argument k and prints the kth
 // from the last string found on standard input (assuming that standard input has k or
 // more strings).
 public class Exercise15 {
-  private static Queue<String> queue = new Queue<>();
+    private static Out out = new StdOut();
+    private static Queue<String> queue = new Queue<>();
 
-  public static void main(String[] args) {
-    int k = Integer.parseInt(args[0]);
+    public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
 
-    while(!StdIn.isEmpty()) {
-      if (!queue.isEmpty() && queue.size() == k + 1) queue.dequeue();
-      queue.enqueue(StdIn.readLine());
+        while (!StdIn.isEmpty()) {
+            if (!queue.isEmpty() && queue.size() == k + 1) queue.dequeue();
+            queue.enqueue(StdIn.readLine());
+        }
+
+        out.println(queue.dequeue());
     }
 
-    StdOut.println(queue.dequeue());
-  }
+    public static void test() {
+        OutputInterceptor interceptor = new OutputInterceptor();
+        out = interceptor;
+
+        Test.simulateInput(
+          "asdf\n" +
+          "qwer\n" +
+          "uiop\n"
+        );
+
+        main(new String[] { "2" });
+
+        Test.assertEqual(interceptor.contents(), "asdf\n");
+
+        interceptor.reset();
+
+        Test.simulateInput(
+          "asdf\n" +
+          "qwer\n" +
+          "uiop\n"
+        );
+
+        main(new String[] { "1" });
+
+        Test.assertEqual(interceptor.contents(), "qwer\n");
+    }
 }
